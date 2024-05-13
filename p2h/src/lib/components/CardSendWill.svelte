9d2f04d3a6da857211372192998c3587ebf9c3dc
Alex <alxwnth@posteo.me>
2024-05-13 21:02:22 +0300

    feat: Add UI for will sending

---
+++ p2h/src/lib/components/CardSendWill.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { keyPairStore } from '$lib/stores/keyPairStore';
+	import Button from './ui/button/button.svelte';
+
+	import CardContent from './ui/card/card-content.svelte';
+	import CardHeader from './ui/card/card-header.svelte';
+	import CardTitle from './ui/card/card-title.svelte';
+	import Card from './ui/card/card.svelte';
+	import Input from './ui/input/input.svelte';
+	import Label from './ui/label/label.svelte';
+	import Textarea from './ui/textarea/textarea.svelte';
+</script>
+
+<Card>
+	<CardHeader>
+		<CardTitle>Send will</CardTitle>
+	</CardHeader>
+	<CardContent class="flex flex-col gap-4">
+		<Label for="willText" />
+		<Textarea id="willText" name="willText" placeholder="Please, treat this inheritance well..." />
+		<Label for="senderPrivateKey">Your private key</Label>
+		<Input
+			id="senderPrivateKey"
+			name="senderPrivateKey"
+			value={$keyPairStore?.privateKeyHex()}
+			disabled
+		/>
+		<Label for="recipientPublicKey">Recipient's public key</Label>
+		<Input id="recipientPublicKey" name="recipientPublicKey" />
+		<Button disabled>Send</Button>
+	</CardContent>
+</Card>
--- p2h/src/routes/+page.svelte
+++ p2h/src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import CardGenerateKeypair from '$lib/components/CardGenerateKeyPair.svelte';
 	import CardImportKeyPair from '$lib/components/CardImportKeyPair.svelte';
+	import CardSendWill from '$lib/components/CardSendWill.svelte';
 </script>
 
 <CardGenerateKeypair />
 <CardImportKeyPair />
+<CardSendWill />
