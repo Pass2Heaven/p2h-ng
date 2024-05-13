d8f94cd6e8336aad3ae4cf324c1a1da074ea5b90
Alex <alxwnth@posteo.me>
2024-05-13 21:40:27 +0300

    feat: Add navbar

---
+++ p2h/src/lib/components/ui/navbar.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Logo from '$lib/components/svg/logo.svelte';
+</script>
+
+<nav class="flex items-center justify-items-start">
+	<div class="mr-4 max-w-[250px]"><Logo /></div>
+	<div class="space-x-4 lg:space-x-6">
+		<a
+			href="/"
+			class="text-muted-foreground hover:text-primary text-sm font-medium transition-colors"
+		>
+			New will
+		</a>
+		<a
+			href="/"
+			class="text-muted-foreground hover:text-primary text-sm font-medium transition-colors"
+		>
+			Will Vault
+		</a>
+		<a
+			href="/"
+			class="text-muted-foreground hover:text-primary text-sm font-medium transition-colors"
+		>
+			About
+		</a>
+	</div>
+	<div class="ml-auto flex items-center space-x-4">Wallet placeholder</div>
+</nav>
--- p2h/src/routes/+layout.svelte
+++ p2h/src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script>
+	import Navbar from '$lib/components/ui/navbar.svelte';
 	import '../app.css';
 </script>
 
-<slot></slot>
+<div class="container">
+	<div class="mb-4 mt-4">
+		<Navbar />
+	</div>
+	<slot></slot>
+</div>
 
 <style></style>
